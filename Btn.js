a2931769adf4f3b6afc0ec081785d2932ac81238
surbhi <surbhi.singh.ss05@gmail.com>
2018-02-13 12:26:39 +0530

    cng

--- Btn.js
+++ Btn.js
@@ -1,10 +1,11 @@
 //var casper = require('casper').create();
 
 
-casper.test.begin('The heading exists', 2, function suite(test) {
+casper.test.begin('The heading exists', 3, function suite(test) {
     casper.start('http://localhost:8080/webProject1/home.jsp', function() {
     	test.assertExists('button.btn');
     	test.assertSelectorHasText('button.btn', 'Send');
+    	test.assertSelectorHasText('p.text-center', 'We love fans');
     }).run(function() {
         test.done();
     });
