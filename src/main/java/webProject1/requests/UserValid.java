e96a9f3cba3fcd5d85e6f27818cf37b7fc1b2f96
surbhi <surbhi.singh.ss05@gmail.com>
2018-03-01 12:53:52 +0530

    chng

--- src/main/java/webProject1/requests/UserValid.java
+++ src/main/java/webProject1/requests/UserValid.java
@@ -56,6 +56,7 @@ public class UserValid extends HttpServlet {
 			HttpSession session=request.getSession();  
 	        session.setAttribute("name",result);  
 			request.setAttribute("name", result);
+			out.println("<h1>Hello</h1>");
 			request.getRequestDispatcher("home.jsp").include(request, response);
 			out.println("<h1>Hello</h1>");
 			//request.getRequestDispatcher("profile.jsp").forward(request, response);
